ef6016d9cbdc0e9ebf0e194374aae52b1c65756d
liuzhen <liuzhen1010xyz@gmail.com>
2022-11-01 16:07:26 +0800

    feat(react): Concurrent Mode

--- build-your-own-react/react.js
+++ build-your-own-react/react.js
@@ -56,3 +56,18 @@ const element = Didact.createElement(
 // )
 const container = document.getElementById("root")
 Didact.render(element, container)
+
+
+let nextUnitOfWork = null // 当前等待执行的工作单元
+function workLoop(deadline) {
+  let shouldYield = false // 是否有同步执行任务
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)// 执行工作单元
+    shouldYield = deadline.timeRemaining() < 1 // 时间线上的任务剩余时间小于1，认为有同步执行的任务
+  }
+  requestIdleCallback(workLoop)// 任务队列为空 || 有同步任务时 推迟到下一浏览器空闲时执行
+}
+requestIdleCallback(workLoop)// React中使用scheduler package
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO return nextUnitOfWork
+}
