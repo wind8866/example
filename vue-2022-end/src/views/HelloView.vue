0b55114481b281c39e57dbee363e08792cb47e70
liuzhen <liuzhen1010xyz@gmail.com>
2022-12-13 18:21:53 +0800

    feat(vue): demo

---
+++ vue-2022-end/src/views/HelloView.vue
@@ -0,0 +1,24 @@
+<template>
+  <span
+    :attr-title="`${title}-1`"
+    v-bind:title="title"
+    v-bind="objectOfAttrs"
+  >模板语法 {{title}}</span>
+
+  <button :[`attr-${count}`]="count" @click="++count">add {{count}}</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title: string = 'title'
+interface ObjectOfAttrs {
+  id: string
+  class: string
+}
+const objectOfAttrs: ObjectOfAttrs = {
+  id: 'container',
+  class: 'wrapper'
+}
+const count = ref(0)
+
+</script>
